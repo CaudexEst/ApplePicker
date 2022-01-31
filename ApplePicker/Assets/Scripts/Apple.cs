321fd703a292d582613a9f1afd2c328e43991e90
Ben Jenkins <73377556+CaudexEst@users.noreply.github.com>
2022-01-31 16:41:42 -0600

    made the tree and apple

---
+++ ApplePicker/Assets/Scripts/Apple.cs
@@ -0,0 +1,31 @@
+/*
+ * Created by: Ben Jenkins
+ * Date created: 1/31/2022
+ * Last Edited by: NA
+ * Last edited: NA
+ * Discription: Apple despawn logic
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ ApplePicker/Assets/Scripts/Tree.cs
@@ -0,0 +1,62 @@
+/*
+ * Created by: Ben Jenkins
+ * Date created: 1/31/2022
+ * Last Edited by: NA
+ * Last edited: NA
+ * Discription: Controls tree movement and apple spawning
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 24f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+    
+    
+    
+    // Start is called before the first frame update 
+    
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move every tree
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);//move right
+        }
+        else if(pos.x >leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);//move left
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //chanceToChangeDirections directions 
+        }
+    }
+}
